e57abb00fe97533e1170f2a32a9caffa992e4f91
RMontiel9 <rickmontielcallpa19@gmail.com>
2025-05-28 13:16:27 -0400

    Proyecto re-estructuradoo

--- books/views.py
+++ books/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from dotenv import load_dotenv
+import os
+import requests
 # Create your views here.
+
+load_dotenv()
+
+api_key = os.getenv('GOOGLE_BOOKS_API_KEY')
+
+def buscar_libros():
+    query = "django"
+    url = f"https://www.googleapis.com/books/v1/volumes?q={query}&key={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+
+    # Puedes ver el contenido
+    for item in data.get("items", []):
+        title = item["volumeInfo"].get("title", "Sin título")
+        print("Título:", title)
